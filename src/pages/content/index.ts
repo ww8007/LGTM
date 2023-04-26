7d60f5fab370b6eefff413b77e0dfb6207144dec
ww8007 <wshmin1234@gmail.com>
2023-04-26 23:30:47 +0900

    feat: pull Request 받아오는 로직 추가 1. iffe 주입 2. GET_PR_LIST 처리 3. APPROVE_PR 처리

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,15 +1,42 @@
 import approvePR from "./modules/approvePR";
+import injectScript from "./modules/injectScript";
 
-console.log("content loaded");
+injectScript(
+  chrome.runtime.getURL("fetchPRList.iife.js"),
+  document.documentElement
+);
 
-/**
- * @description
- * Chrome extensions don't support modules in content scripts.
- */
-import("./components/Demo");
+const fetchPRListFromPage = async (): Promise<
+  Array<{ title: string; issueNumber: string }>
+> => {
+  return new Promise((resolve) => {
+    window.postMessage({ type: "FETCH_PR_LIST" }, window.location.origin);
+    window.addEventListener("message", (event) => {
+      if (
+        event.source === window &&
+        event.data.type === "FETCH_PR_LIST_RESULT"
+      ) {
+        resolve(event.data.prList);
+      }
+    });
+  });
+};
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.action === "approvePR") {
-    await approvePR();
-  }
-});
+if (window.location.hostname === "github.com") {
+  chrome.runtime.onMessage.addListener(async (request) => {
+    if (request.message === "GET_PR_LIST") {
+      const prList = await fetchPRListFromPage();
+      chrome.runtime.sendMessage({
+        code: 200,
+        message: "PR_LIST",
+        data: prList,
+      });
+      return true;
+    }
+    if (request.message === "APPROVE_PR") {
+      await approvePR(request.data.issueNumber);
+      chrome.runtime.sendMessage({ code: 200, message: "APPROVED" });
+    }
+    return true;
+  });
+}
