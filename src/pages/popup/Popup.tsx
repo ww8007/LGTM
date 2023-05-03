00368bee1812ce6a699695f8ec365c4e886c14e0
ww8007 <wshmin1234@gmail.com>
2023-05-03 20:04:39 +0900

    refactor: 기본 css @vanilla-extract로 마이그레이션 진행

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,6 +1,6 @@
-import "@pages/popup/Popup.css";
 import { useEffect, useState } from "react";
 import logo from "@assets/img/Logo.png";
+import { appStyles } from "./styles/popup.css";
 const Popup = () => {
   const [prList, setPRList] = useState<PullRequestItem[]>([]);
 
@@ -13,11 +13,9 @@ const Popup = () => {
   });
 
   useEffect(() => {
-    setTimeout(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { message: "GET_PR_LIST" });
-      });
-    }, 1000);
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { message: "GET_PR_LIST" });
+    });
   }, []);
 
   const onClickApprovePR = (issueNumber: string) => {
@@ -30,22 +28,28 @@ const Popup = () => {
   };
 
   return (
-    <div className="App">
+    <div className={appStyles.app}>
       <header>
-        <img src={logo} width={120} className="App-logo" alt="logo" />
-        <ul className="pr-list">
+        <img src={logo} width={120} alt="logo" />
+        <ul className={appStyles.prList}>
           {prList.map((pr) => (
             <button
               onClick={() => onClickApprovePR(pr.issueNumber)}
-              className="pr-item"
+              className={appStyles.prItem}
               key={pr.issueNumber}
             >
-              <div className="pr_flex_row">
-                <img src={pr.avatarUrl} alt="" />
-                <div className="pr-item__info__author">{pr.username}</div>
+              <div className={appStyles.prFlexRow}>
+                <img
+                  className={appStyles.userProfile}
+                  src={pr.avatarUrl}
+                  alt="git profile image"
+                />
+                <span className={appStyles.prItemInfoAuthor}>
+                  {pr.username}
+                </span>
               </div>
-              <div className="pr-item__info">
-                <div className="pr-item__info__title">{pr.title}</div>
+              <div className={appStyles.prItemInfo}>
+                <div className={appStyles.prItemInfoTitle}>{pr.title}</div>
               </div>
             </button>
           ))}
